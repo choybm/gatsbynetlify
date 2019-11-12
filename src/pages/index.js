b3b9b8b891d60a6b70921fd6dbf2a78790cfac0f
brch1381exc <brch1381exc@colorado.edu>
2019-11-12 15:17:03 -0700

    live site test

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <p>Testing testing 1 2 3</p>
+    <p>Testing local</p>
+    <p>Testing live site changes</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
